059150c4badc117f4c92c91d6b6bc0f686863a80
Abhi0790 <142735607+Abhi0790@users.noreply.github.com>
2024-08-17 13:46:34 +0530

    Update cppstl.cpp

--- cppstl.cpp
+++ cppstl.cpp
@@ -1,1 +1,21 @@
+#include <bits/stdc++> //all the libraries included
+using namespace std //helps in variable description "cin >> a;" would be valid only if you write namespace std at the top else for declaration youll have to type "std::cin >> a; " 
 
+void print()// void function doesnt return any value
+{
+cout << "raj";
+}
+
+int sum(int a, int b)
+{
+return a+b;
+}
+
+int main(){
+  print();
+int s= sum (4,5)
+cout << s;
+return 0;
+}
+
+  
